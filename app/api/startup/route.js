4cce8d0112147f955ce170771f7d7142ad503bc0
Abdallah Shafiullah <ashafi@bu.edu>
2025-08-07 17:27:21 -0400

    Removed profile router

--- app/api/startup/route.js
+++
@@ -1,8 +0,0 @@
-import { clearProfile } from '@/lib/profileLib/clearProfile';
-
-
-clearProfile();
-
-export async function GET() {
-    return new Response('Start has already been run.', { status: 200 });
-}
